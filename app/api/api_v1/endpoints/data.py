0ee2f2bf7392e372a496cd10a36e6c4074383233
Retham Lai <rethamlai@Rethams-MBP.lan>
2023-05-21 14:39:52 +1000

    testing postgres

--- app/api/api_v1/endpoints/data.py
+++ app/api/api_v1/endpoints/data.py
@@ -21,9 +21,44 @@ async def root():
         user = username,
         password = password
     )
+    conn.autocommit = True
     cur = conn.cursor(cursor_factory=RealDictCursor)
     cur.execute('''select now()''')
     results = cur.fetchall()
     # json_result = json.dumps(results)
     # print(json_result)
     return {"message": results}
+
+@router.get("/create")
+async def create_db():
+    conn = psycopg2.connect(
+        host = host,
+        port = port,
+        database = database,
+        user = username,
+        password = password
+    )
+    conn.autocommit = True
+    cur = conn.cursor()
+    cur.execute('''create table customerInfo (id SERIAL PRIMARY KEY, name text, age float)''')
+    # results = cur.fetchall()
+    # json_result = json.dumps(results)
+    # print(json_result)
+    return {"message": "successfully created"}
+
+@router.get("/insert")
+async def insert_db():
+    conn = psycopg2.connect(
+        host = host,
+        port = port,
+        database = database,
+        user = username,
+        password = password
+    )
+    conn.autocommit = True
+    cur = conn.cursor()
+    cur.execute('''insert into customerInfo (name, age) values ('abc', extract(minute from timestamp now()))''')
+    # results = cur.fetchall()
+    # json_result = json.dumps(results)
+    # print(json_result)
+    return {"message": "successfully inserted"}
